7c483b059273611850b835df84a5b4b0d16bad3b
LeSnow-Ye <lesnow.ye@gmail.com>
2025-01-09 16:52:29 +0800

    Add disk mesh.

--- mesh.py
+++ mesh.py
@@ -86,6 +86,86 @@ def generate_square_mesh(
     gmsh.finalize()
 
 
+def generate_disk_mesh(
+    radius, size_min, size_max, sc_boundary=0.0, filename="disk.msh", gui=False
+):
+    gmsh.initialize()
+    gmsh.clear()
+    gmsh.model.add("disk")
+
+    disk = gmsh.model.occ.addDisk(0, 0, 0, radius, radius)
+
+    sc_dimtags = []
+    if sc_boundary > 0.0:
+        sc_raw = gmsh.model.occ.addDisk(
+            0, 0, 0, radius + sc_boundary, radius + sc_boundary
+        )
+        sc_dimtags, _ = gmsh.model.occ.cut([(2, sc_raw)], [(2, disk)], -1, True, False)
+
+    gmsh.model.occ.synchronize()
+
+    # Physical groups
+    gmsh.model.addPhysicalGroup(2, [disk], -1, "Cavity")
+    gmsh.model.addPhysicalGroup(
+        2, [x[1] for x in sc_dimtags if x[0] == 2], -1, "SC_boundary"
+    )
+
+    # Set mesh size
+    points = [
+        x[1]
+        for x in gmsh.model.getBoundary([(2, disk)], False, True, True)
+        if x[0] == 0
+    ]
+    curves = [
+        x[1]
+        for x in gmsh.model.getBoundary([(2, disk)], False, False, False)
+        if x[0] == 1
+    ]
+
+    print("Points:", points)
+    print("Curves:", curves)
+
+    gmsh.option.setNumber("Mesh.MeshSizeMin", size_min)
+    gmsh.option.setNumber("Mesh.MeshSizeMax", size_max)
+    gmsh.option.setNumber("Mesh.MeshSizeFromPoints", 0)
+    gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 0)
+    gmsh.option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
+
+    gmsh.model.mesh.field.add("Distance", 1)
+    gmsh.model.mesh.field.setNumbers(1, "PointsList", points)
+    gmsh.model.mesh.field.setNumbers(1, "CurvesList", curves)
+    gmsh.model.mesh.field.setNumber(1, "Sampling", radius / size_min)
+
+    gmsh.model.mesh.field.add("Threshold", 2)
+    gmsh.model.mesh.field.setNumber(2, "InField", 1)
+    gmsh.model.mesh.field.setNumber(2, "SizeMin", size_min)
+    gmsh.model.mesh.field.setNumber(2, "SizeMax", size_max)
+    gmsh.model.mesh.field.setNumber(2, "DistMin", 3 * size_min)
+    gmsh.model.mesh.field.setNumber(2, "DistMax", 0.2 * radius)
+
+    gmsh.model.mesh.field.add("Min", 101)
+    gmsh.model.mesh.field.setNumbers(101, "FieldsList", [2])
+    gmsh.model.mesh.field.setAsBackgroundMesh(101)
+
+    gmsh.option.setNumber("Mesh.Algorithm", 6)
+    gmsh.model.mesh.generate(2)
+
+    # Save mesh
+    if not os.path.exists("mesh"):
+        os.makedirs("mesh")
+
+    gmsh.option.setNumber("Mesh.MshFileVersion", 2.2)
+    gmsh.option.setNumber("Mesh.Binary", 0)
+    gmsh.write(os.path.join("mesh", filename))
+
+    # Optionally launch GUI
+    if gui:
+        gmsh.fltk.run()
+
+    gmsh.finalize()
+
+
 if __name__ == "__main__":
     # generate_square_mesh(1.0, 1.0, 10, 10)
-    generate_square_mesh(1.0, 1.0, 0.005, 0.05, 0.5, "square_sc_boundary.msh", True)
+    # generate_square_mesh(1.0, 1.0, 0.005, 0.05, 0.5, "square_sc_boundary.msh", True)
+    generate_disk_mesh(1.0, 0.01, 0.05, 0.5, "disk_sc_boundary.msh", True)
